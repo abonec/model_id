6dad6d3f882f75f80206982589246ad5648056ce
abonec <abonec@gmail.com>
2016-04-30 15:23:29 +0300

    add workaround for old rubies

--- lib/model_id/base.rb
+++ lib/model_id/base.rb
@@ -1,30 +1,31 @@
 module ModelId
   module Base
+    def model_id
+      @model_id
+    end
+
+    def set_next_model_id
+      @model_id = self.class.last_model_id + 1
+      self.class.last_model_id = @model_id
+    end
+
     def self.included(base)
       if base.respond_to? :prepend
-        base.prepend InstanceMethods
+        base.prepend Ruby2Initializer
         base.class_eval do
           class << self
             attr_accessor :last_model_id
           end
         end
         base.last_model_id = 0
-      else
-        raise 'ModelId supports only ruby 2.x now'
       end
     end
 
-    module InstanceMethods
-      def model_id
-        @model_id
-      end
-
+    module Ruby2Initializer
       def initialize(*args)
-        @model_id = self.class.last_model_id + 1
-        self.class.last_model_id = @model_id
+        set_next_model_id
         super(*args)
       end
-
     end
   end
 end
